a7274ad543a0d514db10913a4cc9c62ee1595bf9
Zack Whiting <zackwhit694@ed.amdsb.ca>
2024-09-27 12:45:12 -0400

    Update basic.cmd

--- basic.cmd
+++ basic.cmd
@@ -1,6 +1,24 @@
 @echo off
 Title Basic Calculator
 :main (
-echo +, -, *, /?
-  set /p "operation=->
-  )
+  cls
+  echo +, -, *, /?
+    set /p "operation=->"
+  echo How many numbers to calculate with? (10 max) (Input an integer only)
+    set /p "operationNum->"
+  if %operationNum% LEQ 0 goto invNum
+  if %operationNum% GTR 10 goto invNum
+  if %operationNum% GEQ 1 if %operationNum% LEQ 10 goto main2
+  echo invalid
+  pause
+  goto main
+)
+
+:invNum (
+  if %operationNum% LEQ 0 echo Number must be > 0
+  pause
+goto main
+  if %operationNum% GTR 10 echo Number must be < or = 10
+  pause
+goto main
+)
